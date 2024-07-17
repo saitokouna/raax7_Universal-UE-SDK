971e679c938f2b76d972f63c67268f02b15f7be4
raax7 <cobyispoopu@gmail.com>
2024-07-17 23:35:50 +0100

    Added includes in SDK.hpp.

--- include/SDK.hpp
+++ include/SDK.hpp
@@ -1,5 +1,9 @@
 #pragma once
 #include <SDKStatus.hpp>
+#include <ugsdk/FMemory.hpp>
+#include <ugsdk/ObjectArray.hpp>
+#include <ugsdk/UnrealObjects.hpp>
+#include <ugsdk/UnrealTypes.hpp>
 
 namespace SDK
 {
